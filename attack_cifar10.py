4c9a11f3abf51091bb7046f081cdee1825d779ca
Mahdiplj <m.dousti1997@gmail.com>
2023-05-27 00:03:44 +0330

    tf.reset graph dep

--- attack_cifar10.py
+++ attack_cifar10.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
         
 
         config_file = config_path_join(_cf)
-        tf.reset_default_graph()
+        tf.compat.v1.reset_default_graph()
 
         with open(config_file) as config_file:
             config = json.load(config_file)
